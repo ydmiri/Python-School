fa57f53553c5eccee3137f80038f58bd1e894346
ydmiri <ydmiri02@gmail.com>
2017-04-29 12:18:10 +0100

    Add files via upload

---
+++ Cows---Bulls/User 4 Digits.py
@@ -0,0 +1,8 @@
+### - Cows & Bulls - ###
+
+
+## - getusersinput - ##
+
+def getuserinput():
+    userguess = input('\n*NOTICE* - You can always type \'exit\' to quit!\n\nPlease enter a  4 digit number!\nRemember that each digit will only appear once!\n\nNumber:  ')
+getuserinput()
